89d075a92c2ea78beffde38339fc9c7b459cbf7f
abenaderby <boatemaaharobed@gmail.com>
2023-05-04 06:13:50 -0700

    ./5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,7 @@ char *string_toupper(char *n)
 	p = 0;
 	while (n[p] != '\0')
 	{
-		if (n[p] >= 'a' && n[p] <= 'Z')
+		if (n[p] >= 'a' && n[p] <= 'z')
 			n[p] = n[p] - 32;
 		p++;
 	}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string
+ * Return: pointer to string
+ */
+
+char *cap_string(char *s)
+{
+	int word = 0;
+
+	while (s[word])
+	{
+		while (!(s[word] >= 'a' && s[word] <= 'Z'))
+			word++;
+
+		if (s[word - 1] == ' ' ||
+			s[word - 1] == '\t' ||
+			s[word - 1] == '\n' ||
+			s[word - 1] == ',' ||
+			s[word - 1] == ';' ||
+			s[word - 1] == '.' ||
+			s[word - 1] == '.' ||
+			s[word - 1] == '!' ||
+			s[word - 1] == '?' ||
+			s[word - 1] == '"' ||
+			s[word - 1] == '(' ||
+			s[word - 1] == ')' ||
+			s[word - 1] == '{' ||
+			s[word - 1] == '}' ||
+			word == 0)
+
+			s[word] -= 32;
+		word++;
+	}
+
+	return (s);
+}
