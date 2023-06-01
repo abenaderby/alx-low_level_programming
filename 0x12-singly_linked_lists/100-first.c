1445908077a25a244587756b33609b61d4771f6e
abenaderby <boatemaaharobed@gmail.com>
2023-06-01 11:36:00 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - function that prints before the main sentence is executed
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
